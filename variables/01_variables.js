cd379b8d978a47c252c68f355488f646df671985
Kuldeep  Ghorpade <kuldeepghorpade05@gmail.com>
2024-08-17 00:23:29 +0530

    datatypes

---
+++ variables/01_variables.js
@@ -0,0 +1,24 @@
+const accountId = 14558
+let accountemail = "kuldeep@google.com"
+var accountPassword = "2546"
+accountCity = "jaipur"
+
+//accountId = 2
+
+//console.log(accountId);
+
+//console.log(accountPassword);
+
+accountemail = "hc@123.com"
+accountPassword = "12344555"
+accountCity = "bengluru"
+
+let accountState;
+
+
+console.table([accountId, accountemail, accountPassword, accountCity, accountState]);
+
+/*
+prefer not use var - because of issue in block scope and functional scope
+
+*/
---
+++ variables/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all js code as newer version
+
+// alert("hello") //we are using nodejs, not browser
+
+console.log( 3 + 3); //code raedability high
+
+console.log("hitesh")
+
+
+let name = "kuldee"  //string
+let age = 78  //int
+let isloogedin = false   //boolean
+let state;
+
+// number => 2 to 53
+// bigint
+// string => ""
+// boolean => true/false
+// null
+// undefined =>
+// symbol => unique    
+
+// objet
+
+console.log(typeof "tyuiiii")
+console.log(typeof age)
+console.log(typeof null)  //object
+console.log(typeof undefined) //undefined
+
