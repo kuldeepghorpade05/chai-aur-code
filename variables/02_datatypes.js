a870f1624235303134883b9cb41e7d72415fa7bc
Kuldeep  Ghorpade <kuldeepghorpade05@gmail.com>
2024-08-17 11:12:25 +0530

    operations

--- variables/02_datatypes.js
+++ variables/02_datatypes.js
@@ -49,10 +49,42 @@ console.log(valueinnumber) */
 //1 => true; 0 => false 
 // "" => false // "hitesh" => true
 
-let somenumber = 33
+// let somenumber = 33
 
-let stringnumber = String(somenumber)
-console.log(stringnumber)
+// let stringnumber = String(somenumber)
+// console.log(stringnumber)
 
-console.log(typeof stringnumber)
+// console.log(typeof stringnumber)
 
+// ******************* operations ***************************
+
+// let value = 8
+// let negvalue = -value
+
+// console.log(negvalue)
+
+
+/*let str1 = "hello"
+let str2 = " hitesh"
+let str3 = str1 + str2
+console.log(str3)*/
+
+
+// console.log("1"+ 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+// console.log( (6 + 4) * (7 - 5))
+
+// console.log(+true)
+
+// console.log(+"");
+
+// let num1, num2, num3
+// num1 = num2 = num3 = 2 + 2
+
+
+let gamecounter = 100
+gamecounter++;
+console.log(gamecounter);
