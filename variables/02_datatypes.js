9d6933d640b469fe66a62ec8d2d8ae7e38f1826f
Kuldeep  Ghorpade <kuldeepghorpade05@gmail.com>
2024-08-17 10:48:31 +0530

    conversion datatypes

--- variables/02_datatypes.js
+++ variables/02_datatypes.js
@@ -2,9 +2,9 @@
 
 // alert("hello") //we are using nodejs, not browser
 
-console.log( 3 + 3); //code raedability high
+//console.log( 3 + 3); //code raedability high
 
-console.log("hitesh")
+//console.log("hitesh")
 
 
 let name = "kuldee"  //string
@@ -22,8 +22,37 @@ let state;
 
 // objet
 
-console.log(typeof "tyuiiii")
+/* console.log(typeof "tyuiiii")
 console.log(typeof age)
 console.log(typeof null)  //object
 console.log(typeof undefined) //undefined
+
+let score = 456
+
+console.log(typeof score) */
+
+// conversion to number
+/* let valueinnumber = Number(score)
+console.log(typeof valueinnumber)
+console.log(valueinnumber) */
+
+
+// "33" => 33 number
+// "4546thg" => NaN
+// true => 1; =>0
+
+//let isLoggedIn = "1"
+
+//let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+//1 => true; 0 => false 
+// "" => false // "hitesh" => true
+
+let somenumber = 33
+
+let stringnumber = String(somenumber)
+console.log(stringnumber)
+
+console.log(typeof stringnumber)
 
