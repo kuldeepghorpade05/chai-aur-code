af19db371c3262b6c9ce8603ad176dc09f733a7c
Kuldeep  Ghorpade <kuldeepghorpade05@gmail.com>
2024-08-17 17:39:49 +0530

    array methods

---
+++ arrays/arrays.js
@@ -0,0 +1,63 @@
+
+
+
+const arr = [4,5,4,7,6,2,9];
+// console.log(arr[2]);
+
+const myheros = ["shaktiman", "naagraj"];
+
+const myarr2 = new Array(7, 8, 6, 3, 4);
+// console.log(myarr2);
+
+//methods ***********
+
+myarr2.push(78);
+// console.log(myarr2);
+
+myarr2.pop();
+// console.log(myarr2);
+
+myarr2.unshift(9);
+// console.log(myarr2);
+
+myarr2.shift();
+// console.log(myarr2);
+
+// console.log(myarr2.includes(20));
+
+// console.log(myarr2.indexOf(8));
+
+const newarr = myarr2.join();
+// console.log(newarr);
+
+// console.log(typeof newarr);
+
+
+// slice ******************   splice
+
+console.log("A", myarr2);
+
+// const myn1 = myarr2.slice(1,3);
+// console.log(myn1);
+
+// manipulates original array
+const myn1 = myarr2.splice(1,3);
+console.log(myn1);
+
+console.log("B", myarr2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ variables/Strings.js
@@ -0,0 +1,42 @@
+
+
+const name = "hitesh";
+const gamename = new String('hiteshjsjsj');
+const repocount = 50;
+
+// console.log(name + repocount + " kuldeep");
+
+//console.log(`hello my name is ${name} and my repo count is ${repocount}`);
+
+//console.log(gamename)
+
+// console.log(gamename[0])
+// console.log(gamename[2])
+
+// String methods
+
+// console.log(gamename.length);
+// console.log(gamename.toLocaleUpperCase());
+// console.log(gamename.charAt(8));
+// console.log(gamename.indexOf('t'));
+
+
+// const newString = gamename.substring(0, 4);
+// console.log(newString);
+
+// const anotherStringg = gamename.slice(-8, 4)
+// console.log(anotherStringg)
+
+
+
+const newstringone = "     hitesh    ";
+// console.log(newstringone);
+// console.log(newstringone.trim());
+
+const url = "https//hitesh.com/hotesh%20choudhary";
+console.log(url.replace('%20', '-'));
+
+console.log(url.includes('hitesh'));
+
+
+
